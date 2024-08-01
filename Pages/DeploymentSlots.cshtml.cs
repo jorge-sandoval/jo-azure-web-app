a73f9ea358d117d1b398eab8fe4b13ca4d39b388
Jorge O. Sandoval <george.omar@gmail.com>
2024-08-01 15:31:16 -0500

    Added dployment slots page. Testing deployment slots

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace jo_azure_web_app.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
